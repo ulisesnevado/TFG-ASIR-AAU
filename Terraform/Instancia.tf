917fb89975230a544afcd2b465eb609f483ae914
AngelAlvarezGom <angelptrabajo36@gmail.com>
2026-04-16 12:20:48 +0200

    nuevo  test

--- Terraform/Instancia.tf
+++ Terraform/Instancia.tf
@@ -54,13 +54,102 @@ resource "aws_security_group" "flask_instances_sg" {
   }
 }
 
+# -------------------------
+# Load Balancer
+# -------------------------
+resource "aws_lb" "alb" {
+  name               = "flask-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_alb.id]
 
-    resource "aws_instance" "ubuntu_instance" {
-        ami = "ami-0ec10929233384c7f"
-        instance_type = "t2.micro"
-        key_name = "vockey"
-        vpc_security_group_ids = [aws_security_group.flask_instances_sg.id]
-        tags = {
-        Name = "ubuntu-test"
-    }
+  subnets = ["subnet-XXXX", "subnet-YYYY"]
+}
+
+# -------------------------
+# Target Group
+# -------------------------
+resource "aws_lb_target_group" "tg" {
+  name     = "flask-tg"
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = "vpc-XXXX"
+
+  health_check {
+    path = "/"
+    port = "5000"
+  }
+}
+
+# -------------------------
+# Listener
+# -------------------------
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+
+resource "aws_instance" "ubuntu_instance" {
+  ami                    = "ami-0ec10929233384c7f"
+  instance_type          = "t2.micro"
+  key_name               = "vockey"
+  vpc_security_group_ids = [aws_security_group.flask_instances_sg.id]
+
+  user_data_base64 = base64encode(<<-EOF
+    #!/bin/bash
+    set -x
+
+    apt update -y
+    apt upgrade -y
+    apt install -y python3 python3-pip git
+
+    pip3 install flask
+
+    cd /home/ubuntu
+    git clone https://github.com/ulisesnevado/TFG-ASIR-AAU
+    cd TFG-ASIR-AAU
+
+    if [ -f requirements.txt ]; then
+      pip3 install -r requirements.txt
+    fi
+
+    cat <<EOT > /etc/systemd/system/flaskapp.service
+    [Unit]
+    Description=Flask App
+    After=network.target
+
+    [Service]
+    User=ubuntu
+    WorkingDirectory=/home/ubuntu/TFG-ASIR-AAU
+    ExecStart=/usr/bin/python3 /home/ubuntu/TFG-ASIR-AAU/app.py
+    Restart=always
+
+    [Install]
+    WantedBy=multi-user.target
+    EOT
+
+    systemctl daemon-reload
+    systemctl enable flaskapp
+    systemctl start flaskapp
+  EOF
+  )
+
+  tags = {
+    Name = "ubuntu-test"
+  }
+}
+
+# -------------------------
+# Attach EC2 al Target Group
+# -------------------------
+resource "aws_lb_target_group_attachment" "attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.ubuntu_instance.id
+  port             = 5000
 }
