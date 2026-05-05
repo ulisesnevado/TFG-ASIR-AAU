a53634daaf7633c367f86fc08a75a99f528d2d32
alvaroloureiro <alvilousan@gmail.com>
2026-05-05 13:26:02 +0200

    changes to the AMI owner and filters

--- Terraform/compute.tf
+++ Terraform/compute.tf
@@ -1,10 +1,20 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"] # Canonical
+  owners      = ["099720109477"]  # Canonical (Ubuntu oficial)
 
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
