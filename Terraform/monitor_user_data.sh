c15f486f802e51db92e54583bc683fade79fd4a0
alvaroloureiro <alvilousan@gmail.com>
2026-05-13 15:43:24 +0200

    .

--- Terraform/monitor_user_data.sh
+++ Terraform/monitor_user_data.sh
@@ -6,10 +6,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get install -y python3 python3-pip git ansible
 
-# Ansible-pull: clona el repo y ejecuta el playbook del monitor.
-# Toda la configuracion (dependencias, scripts, .pkl, cron) vive en
-# ansible/playbooks/roles/monitor/tasks/main.yml para mantener el mismo
-# patron que las EC2 Flask.
 ansible-pull \
   -U ${github_repo} \
   -d /home/ubuntu/TFG-ASIR-AAU \
